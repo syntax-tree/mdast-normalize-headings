8de000b430751088123f2f7ceae7449135518b22
Titus Wormer <tituswormer@gmail.com>
2020-11-03 15:47:05 +0100

    Update dev-dependencies

--- test/index.js
+++ test/index.js
@@ -4,34 +4,30 @@ var fs = require('fs')
 var path = require('path')
 var test = require('tape')
 var remark = require('remark')
+var remove = require('unist-util-remove-position')
 var normalizeHeadings = require('..')
 
-var proc = remark().use({settings: {position: false}})
-
-test.Test.prototype.check = check
-
 test('Multiple top-level headings', function (t) {
-  t.check('no-headings', 'No-op if there is no headings')
-  t.check('no-titles', 'No-op if there is no top-level headings')
-  t.check('one-title', 'No-op if there is a single top-level heading')
-  t.check('two-titles', 'Makes the second header one level deeper')
-  t.check('more-titles', 'Shifts all other headings one level deeper')
+  check(t, 'no-headings', 'No-op if there is no headings')
+  check(t, 'no-titles', 'No-op if there is no top-level headings')
+  check(t, 'one-title', 'No-op if there is a single top-level heading')
+  check(t, 'two-titles', 'Makes the second header one level deeper')
+  check(t, 'more-titles', 'Shifts all other headings one level deeper')
   t.end()
 })
 
 test('Level 7', function (t) {
-  t.check('hierarchy', 'There is no depth level 7')
+  check(t, 'hierarchy', 'There is no depth level 7')
   t.end()
 })
 
-function check(test, message) {
-  this.deepEqual(
-    normalizeHeadings(load(test + '.in')),
-    load(test + '.out'),
+function check(t, test, message) {
+  var input = fs.readFileSync(path.join(__dirname, 'fixture', test + '.in'))
+  var output = fs.readFileSync(path.join(__dirname, 'fixture', test + '.out'))
+
+  t.deepEqual(
+    remove(normalizeHeadings(remark().parse(input)), true),
+    remove(remark().parse(output), true),
     message
   )
-}
-
-function load(basename) {
-  return proc.parse(fs.readFileSync(path.join(__dirname, 'fixture', basename)))
 }
