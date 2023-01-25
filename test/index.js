ab14525bd14b9014fd57d2f28ed73ae8825386a7
Titus Wormer <tituswormer@gmail.com>
2023-01-25 11:48:11 +0100

    Add tests for exposed identifiers

--- test/index.js
+++ test/index.js
@@ -8,19 +8,22 @@ import test from 'node:test'
 import {fromMarkdown} from 'mdast-util-from-markdown'
 import {removePosition} from 'unist-util-remove-position'
 import {normalizeHeadings} from '../index.js'
+import * as mod from '../index.js'
 
-test('Multiple top-level headings', async () => {
+test('normalizeHeadings', async () => {
+  assert.deepEqual(
+    Object.keys(mod).sort(),
+    ['normalizeHeadings'],
+    'should expose the public api'
+  )
+
   await check('no-headings', 'No-op if there is no headings')
   await check('no-titles', 'No-op if there is no top-level headings')
   await check('one-title', 'No-op if there is a single top-level heading')
   await check('two-titles', 'Makes the second header one level deeper')
   await check('more-titles', 'Shifts all other headings one level deeper')
-})
-
-test('Level 7', async () => {
   await check('hierarchy', 'There is no depth level 7')
 })
-
 /**
  * @param {string} test
  * @param {string} message
