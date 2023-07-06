7f69fe65dfff27d3c3d5d8838936dd9e9e3c8ade
Titus Wormer <tituswormer@gmail.com>
2023-07-06 16:31:01 +0200

    Refactor code-style

--- test/index.js
+++ test/index.js
@@ -8,41 +8,40 @@ import test from 'node:test'
 import {fromMarkdown} from 'mdast-util-from-markdown'
 import {removePosition} from 'unist-util-remove-position'
 import {normalizeHeadings} from '../index.js'
-import * as mod from '../index.js'
 
-test('normalizeHeadings', async () => {
-  assert.deepEqual(
-    Object.keys(mod).sort(),
-    ['normalizeHeadings'],
-    'should expose the public api'
-  )
+test('normalizeHeadings', async function (t) {
+  await t.test('should expose the public api', async function () {
+    assert.deepEqual(Object.keys(await import('../index.js')).sort(), [
+      'normalizeHeadings'
+    ])
+  })
 
-  await check('no-headings', 'No-op if there is no headings')
-  await check('no-titles', 'No-op if there is no top-level headings')
-  await check('one-title', 'No-op if there is a single top-level heading')
-  await check('two-titles', 'Makes the second header one level deeper')
-  await check('more-titles', 'Shifts all other headings one level deeper')
-  await check('hierarchy', 'There is no depth level 7')
+  /** @type {Record<string, string>} */
+  const cases = {
+    'no-headings': 'should be a no-op if there are no headings',
+    'no-titles': 'should be a no-op if there are no top-level headings',
+    'one-title': 'should be a no-op if there is a single top-level heading',
+    'two-titles': 'should make the second header one level deeper',
+    'more-titles': 'should shift all other headings one level deeper',
+    hierarchy: 'should be create a depth level 7'
+  }
+
+  for (const [name, message] of Object.entries(cases)) {
+    await t.test(message, async function () {
+      const input = await fs.readFile(
+        new URL('fixture/' + name + '.in', import.meta.url)
+      )
+      const output = await fs.readFile(
+        new URL('fixture/' + name + '.out', import.meta.url)
+      )
+      const actual = fromMarkdown(input)
+      const expected = fromMarkdown(output)
+      normalizeHeadings(actual)
+
+      assert.deepEqual(
+        removePosition(actual, true),
+        removePosition(expected, true)
+      )
+    })
+  }
 })
-/**
- * @param {string} test
- * @param {string} message
- * @returns {Promise<void>}
- */
-async function check(test, message) {
-  const input = await fs.readFile(
-    new URL('fixture/' + test + '.in', import.meta.url)
-  )
-  const output = await fs.readFile(
-    new URL('fixture/' + test + '.out', import.meta.url)
-  )
-  const actual = fromMarkdown(input)
-  const expected = fromMarkdown(output)
-  normalizeHeadings(actual)
-
-  assert.deepEqual(
-    removePosition(actual, true),
-    removePosition(expected, true),
-    message
-  )
-}
